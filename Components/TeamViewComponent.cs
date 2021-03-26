8951e4e8d1e65ba50cc435480e72f1a6252583c1
haileyhullinger <hh49362@gmail.com>
2021-03-25 21:43:11 -0600

    Add project files.

---
+++ Components/TeamViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Components
+{
+    public class TeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public TeamViewComponent(BowlingLeagueContext con)
+        {
+            context = con;
+        }
+        public IViewComponentResult Invoke()
+        {
+            //Select each unique team object to filter by
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //added for database
+        //private readonly BowlingLeagueContext context;
+        private BowlingLeagueContext context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext con)
+        {
+            _logger = logger;
+            context = con;
+        }
+
+        //if a team name is passed into the view, filter by that team
+        //if a team name is not passed in (nullable with the ?) then nothing will be passed in and all team names show up
+        public IActionResult Index(long? teamid)
+        {
+            return View(context.Bowlers
+                .FromSqlInterpolated($"SELECT * FROM Bowlers WHERE TeamId = {teamid} OR {teamid} IS NULL")
+                .ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
